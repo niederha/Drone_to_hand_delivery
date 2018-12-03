f2dae31d958b2fb3fa9804470325a65bfe589cbf
Felix Nilius <felix.nilius@epfl.ch>
2018-12-03 09:35:57 +0100

    adding transition from create form to WaitingForAcceptationByReceiver and creating a first draft of the later (ie only initial message)

---
+++ mobile/src/main/java/felix_loc_herman/drone_delivery/WaitingForAcceptationByReceiverActivity.java
@@ -0,0 +1,13 @@
+package felix_loc_herman.drone_delivery;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WaitingForAcceptationByReceiverActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_waiting_for_acceptation_by_receiver);
+    }
+}
