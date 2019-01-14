59e843a3019a3f5779c985822c4fc2e7ce9d78a2
Felix Nilius <felix.nilius@epfl.ch>
2019-01-14 21:46:15 +0100

    acorrecting GPS in Map and Receiving activities

--- mobile/src/main/java/felix_loc_herman/drone_delivery/ReceivingActivity.java
+++ mobile/src/main/java/felix_loc_herman/drone_delivery/ReceivingActivity.java
@@ -115,14 +115,11 @@ public class ReceivingActivity extends AppCompatActivity implements OnMapReadyCa
 
             Log.e("ReceivingActivity","Problem with GPS connection");
 
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
+            int TAG_CODE_PERMISSION_LOCATION=42;
+            ActivityCompat.requestPermissions(this, new String[] {
+                            Manifest.permission.ACCESS_FINE_LOCATION,
+                            Manifest.permission.ACCESS_COARSE_LOCATION },
+                    TAG_CODE_PERMISSION_LOCATION);
         }
         else
         {
