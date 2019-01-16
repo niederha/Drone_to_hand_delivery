6ad7d8d1e9e084e1862df857c8ad92e0bb310cf3
Herman Dieset <hdieset@gmail.com>
2019-01-16 21:54:24 +0100

    Hopefully all cancel buttons are working now. Updated app icons.

--- mobile/src/main/java/felix_loc_herman/drone_delivery/MapActivity.java
+++ mobile/src/main/java/felix_loc_herman/drone_delivery/MapActivity.java
@@ -275,10 +275,8 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         {
             //TODO : modify if we want a summary or a history of delivery(ies)
             deliveryRef.setValue(null); //delete the delivery structure
-            Intent intent = new Intent(this,MainActivity.class);
+            Intent intent = new Intent(this,GPSActivity.class);
             intent.putExtra(MainActivity.USERNAME,sender_username);
-            //intent.putExtra("receiver_username",receiver_username);
-            //intent.putExtra("droneHandler",droneHandler);
             startActivity(intent);
 
         }
