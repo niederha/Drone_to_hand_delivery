24b45b92ad3374b311251fa85813674b077b6b64
Loïc Niederhauser <loic.niederhauser@gmail.com>
2019-01-16 16:01:29 +0100

    Implementation and update of the drone handler

--- mobile/src/main/java/felix_loc_herman/drone_delivery/DroneHandler.java
+++ mobile/src/main/java/felix_loc_herman/drone_delivery/DroneHandler.java
@@ -1,93 +1,175 @@
 package felix_loc_herman.drone_delivery;
 
-import com.parrot.arsdk.ARSDK;
+import com.parrot.arsdk.arcommands.ARCOMMANDS_ARDRONE3_PILOTINGSTATE_FLYINGSTATECHANGED_STATE_ENUM;
+import com.parrot.arsdk.arcommands.ARCOMMANDS_COMMON_MAVLINKSTATE_MAVLINKFILEPLAYINGSTATECHANGED_STATE_ENUM;
+import com.parrot.arsdk.arcommands.ARCOMMANDS_COMMON_MAVLINKSTATE_MAVLINKPLAYERRORSTATECHANGED_ERROR_ENUM;
+import com.parrot.arsdk.arcontroller.ARCONTROLLER_DEVICE_STATE_ENUM;
+import com.parrot.arsdk.arcontroller.ARCONTROLLER_DICTIONARY_KEY_ENUM;
+import com.parrot.arsdk.arcontroller.ARCONTROLLER_ERROR_ENUM;
+import com.parrot.arsdk.arcontroller.ARControllerArgumentDictionary;
+import com.parrot.arsdk.arcontroller.ARControllerDictionary;
+import com.parrot.arsdk.arcontroller.ARControllerException;
+import com.parrot.arsdk.arcontroller.ARDeviceController;
+import com.parrot.arsdk.arcontroller.ARDeviceControllerListener;
+import com.parrot.arsdk.arcontroller.ARFeatureARDrone3;
+import com.parrot.arsdk.arcontroller.ARFeatureCommon;
+import com.parrot.arsdk.ardiscovery.ARDISCOVERY_PRODUCT_ENUM;
 import com.parrot.arsdk.ardiscovery.ARDiscoveryDevice;
+import com.parrot.arsdk.ardiscovery.ARDiscoveryDeviceNetService;
 import com.parrot.arsdk.ardiscovery.ARDiscoveryDeviceService;
 import com.parrot.arsdk.ardiscovery.ARDiscoveryException;
-import com.parrot.arsdk.ardiscovery.ARDiscoveryService;
-import com.parrot.arsdk.ardiscovery.receivers.ARDiscoveryServicesDevicesListUpdatedReceiver;
-import com.parrot.arsdk.ardiscovery.receivers.ARDiscoveryServicesDevicesListUpdatedReceiverDelegate;
+import com.parrot.arsdk.arsal.ARSALPrint;
 
-
-import android.content.BroadcastReceiver;
 import android.content.Context;
-import android.content.Intent;
-import android.content.ComponentName;
-import android.content.IntentFilter;
-import android.content.ServiceConnection;
-import android.os.IBinder;
-import android.support.annotation.NonNull;
-import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 
-import java.io.Serializable;
-import java.util.List;
 
 import static android.content.ContentValues.TAG;
 
-public class DroneHandler implements Serializable {
+public class DroneHandler implements ARDeviceControllerListener {
 
-    private ARDiscoveryServicesDevicesListUpdatedReceiver receiver;
+    private ARDeviceController mDroneController;
+    private ARDiscoveryDeviceService mService;
+    private ARCOMMANDS_COMMON_MAVLINKSTATE_MAVLINKFILEPLAYINGSTATECHANGED_STATE_ENUM autoFlightState;
+    static private droneState state = droneState.NOT_CONNECTED;
 
-    /*public enum droneState{
-        IDLE, GOING_TOR_ECIVER, WATINTG_TO_LAND, LANDED_AT_RECIEVER, GOING_BACK
-    }*/
-    public static final int IDLE=0;
-    public static final int FLYING=1;
-    public static final int WAITING_TO_LAND=2;
-    public static final int LANDING=3;
-    public static final int LANDED=4;
+    public enum droneState{
+        IDLE, FLYING, WAITING_TO_LAND, LANDED,NOT_CONNECTED
+    }
 
+    public DroneHandler(ARDiscoveryDeviceService service) {
+        state = droneState.NOT_CONNECTED;
+        mService = service;
+        ARDiscoveryDevice device = createDiscoveryDevice();
+        mDroneController = createController(device);
+        if (mDroneController != null) {
+            mDroneController.addListener(this);
+            ARCONTROLLER_ERROR_ENUM error = mDroneController.start();
+            mDroneController.getFeatureCommon().sendWifiSettingsOutdoorSetting((byte)1);
+            state = droneState.IDLE;
+        }
+        else{
+            Log.e(TAG, "NO CONTROLLER");
+        }
 
-    public DroneHandler(Context context){
-        ARSDK.loadSDKLibs();
     }
 
     //region PublicFunctions
 
-    public int getDroneState(){
-        return 0;
+    public boolean isConnectedToADrone(){
+        if (mDroneController != null){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 
-    // Get the device list to use
-    public int getDeviceList(){
-        int mock = 0;
-        return mock;
-    }
-
-    // Connects the object to the drone
-    public boolean connectToDrone(int device){
-        return true;
-    }
-
-    public void takeOff (){
-
-    }
-
-    public boolean isConnectedToDrone()
+    public void takeOff()
     {
-        return true;
+        if (ARCOMMANDS_ARDRONE3_PILOTINGSTATE_FLYINGSTATECHANGED_STATE_ENUM.ARCOMMANDS_ARDRONE3_PILOTINGSTATE_FLYINGSTATECHANGED_STATE_LANDED.equals(getPilotingState()))
+        {
+            ARCONTROLLER_ERROR_ENUM error = mDroneController.getFeatureARDrone3().sendPilotingTakeOff();
+
+            if (!error.equals(ARCONTROLLER_ERROR_ENUM.ARCONTROLLER_OK))
+            {
+                ARSALPrint.e(TAG, "Error while sending take off: " + error);
+            }
+            else{
+                state = droneState.FLYING;
+            }
+        }
+
     }
 
-    public void goTo( double north, double east){
+    public void land()
+    {
+        ARCOMMANDS_ARDRONE3_PILOTINGSTATE_FLYINGSTATECHANGED_STATE_ENUM flyingState = getPilotingState();
+        if (ARCOMMANDS_ARDRONE3_PILOTINGSTATE_FLYINGSTATECHANGED_STATE_ENUM.ARCOMMANDS_ARDRONE3_PILOTINGSTATE_FLYINGSTATECHANGED_STATE_HOVERING.equals(flyingState) ||
+                ARCOMMANDS_ARDRONE3_PILOTINGSTATE_FLYINGSTATECHANGED_STATE_ENUM.ARCOMMANDS_ARDRONE3_PILOTINGSTATE_FLYINGSTATECHANGED_STATE_HOVERING.equals(flyingState))
+        {
+            ARCONTROLLER_ERROR_ENUM error = mDroneController.getFeatureARDrone3().sendPilotingLanding();
+
+            if (!error.equals(ARCONTROLLER_ERROR_ENUM.ARCONTROLLER_OK))
+            {
+                ARSALPrint.e(TAG, "Error while sending take off: " + error);
+            }
+            else{
+                state = droneState.FLYING;
+            }
+
+        }
+    }
+
+    static public droneState getDroneState(){
+        if (state == null){
+            return droneState.NOT_CONNECTED;
+        }
+        else{
+            return state;
+        }
+    }
+
+
+    public void goTo(float latitude, float  longitude, Context mContext)
+    {
 
     }
 
-    public void land(){
-
+    @Override
+    public void onExtensionStateChanged (ARDeviceController deviceController, ARCONTROLLER_DEVICE_STATE_ENUM newState, ARDISCOVERY_PRODUCT_ENUM product, String name, ARCONTROLLER_ERROR_ENUM error)
+    {
+        return;
     }
 
-    public void getBack(){
+    @Override
+    public void onCommandReceived (ARDeviceController deviceController, ARCONTROLLER_DICTIONARY_KEY_ENUM commandKey, ARControllerDictionary elementDictionary) {
+        if ((commandKey == ARCONTROLLER_DICTIONARY_KEY_ENUM.ARCONTROLLER_DICTIONARY_KEY_COMMON_MAVLINKSTATE_MAVLINKPLAYERRORSTATECHANGED) && (elementDictionary != null)){
+            ARControllerArgumentDictionary<Object> args = elementDictionary.get(ARControllerDictionary.ARCONTROLLER_DICTIONARY_SINGLE_KEY);
+            if (args != null) {
+                ARCOMMANDS_COMMON_MAVLINKSTATE_MAVLINKPLAYERRORSTATECHANGED_ERROR_ENUM error = ARCOMMANDS_COMMON_MAVLINKSTATE_MAVLINKPLAYERRORSTATECHANGED_ERROR_ENUM.getFromValue((Integer)args.get(ARFeatureCommon.ARCONTROLLER_DICTIONARY_KEY_COMMON_MAVLINKSTATE_MAVLINKPLAYERRORSTATECHANGED_ERROR));
+                Log.e(TAG, error.toString());
+            }
+        }
 
+        if (commandKey == ARCONTROLLER_DICTIONARY_KEY_ENUM.ARCONTROLLER_DICTIONARY_KEY_ARDRONE3_PILOTINGSTATE_POSITIONCHANGED && (elementDictionary != null)){
+            ARControllerArgumentDictionary<Object> args = elementDictionary.get(ARControllerDictionary.ARCONTROLLER_DICTIONARY_SINGLE_KEY);
+            if (args != null) {
+                double latitude = (double)args.get(ARFeatureARDrone3.ARCONTROLLER_DICTIONARY_KEY_ARDRONE3_PILOTINGSTATE_POSITIONCHANGED_LATITUDE);
+                double longitude = (double)args.get(ARFeatureARDrone3.ARCONTROLLER_DICTIONARY_KEY_ARDRONE3_PILOTINGSTATE_POSITIONCHANGED_LONGITUDE);
+                double altitude = (double)args.get(ARFeatureARDrone3.ARCONTROLLER_DICTIONARY_KEY_ARDRONE3_PILOTINGSTATE_POSITIONCHANGED_ALTITUDE);
+            }
+        }
+
+        if ((commandKey == ARCONTROLLER_DICTIONARY_KEY_ENUM.ARCONTROLLER_DICTIONARY_KEY_COMMON_COMMONSTATE_BATTERYSTATECHANGED) && (elementDictionary != null)){
+            ARControllerArgumentDictionary<Object> args = elementDictionary.get(ARControllerDictionary.ARCONTROLLER_DICTIONARY_SINGLE_KEY);
+            if (args != null) {
+                byte percent = (byte)((Integer)args.get(ARFeatureCommon.ARCONTROLLER_DICTIONARY_KEY_COMMON_COMMONSTATE_BATTERYSTATECHANGED_PERCENT)).intValue();
+            }
+        }
     }
 
-    public int getFlightStatus(){
-        int flightStatus = 0;
-        return flightStatus;
+    @Override
+    // called when the state of the device controller has changed
+    public void onStateChanged (ARDeviceController deviceController, ARCONTROLLER_DEVICE_STATE_ENUM newState, ARCONTROLLER_ERROR_ENUM error)
+    {
+        switch (newState)
+        {
+            case ARCONTROLLER_DEVICE_STATE_RUNNING:
+                break;
+            case ARCONTROLLER_DEVICE_STATE_STOPPED:
+                break;
+            case ARCONTROLLER_DEVICE_STATE_STARTING:
+                break;
+            case ARCONTROLLER_DEVICE_STATE_STOPPING:
+                break;
+            default:
+                break;
+        }
     }
 
     // Return the current ETA
-    public static double getETAmin(){
+    public double getETAmin(){
         return 1;
     }
 
@@ -96,111 +178,67 @@ public class DroneHandler implements Serializable {
         return 1;
     }
     //endregion
-    /*
-    private ARDiscoveryService mArdiscoveryService = null;
-    private ServiceConnection mArdiscoveryServiceConnection = null; // Connection to the drone
 
-    private final ARDiscoveryServicesDevicesListUpdatedReceiverDelegate mDiscoveryDelegate =
-            new ARDiscoveryServicesDevicesListUpdatedReceiverDelegate() {
-
-                @Override
-                public void onServicesDevicesListUpdated() {
-                    if (mArdiscoveryService != null) {
-                        // List containing all drones
-                        List<ARDiscoveryDeviceService> deviceList = mArdiscoveryService.getDeviceServicesArray();
-                        // TODO: something with the list
-                    }
-                }
-            };
-
-    // Starting the drone discovery
-    private void initDiscoveryService()
+    private ARDeviceController createController(ARDiscoveryDevice device)
     {
-        // create the service connection
-        if (mArdiscoveryServiceConnection == null)
-        {
-            mArdiscoveryServiceConnection = new ServiceConnection() {
-                @Override
-                public void onServiceConnected(ComponentName name, IBinder service) {
-                    mArdiscoveryService = ((ARDiscoveryService.LocalBinder) service).getService();
-                    startDiscovery();
-                }
-
-                @Override
-                public void onServiceDisconnected(ComponentName name) {
-                    mArdiscoveryService = null;
-                }
-            };
-
-        }
-
-        if (mArdiscoveryService == null)
-        {
-            // if the discovery service doesn't exists, bind to it
-            Intent i = new Intent(context, ARDiscoveryService.class);
-            context.bindService(i, mArdiscoveryServiceConnection, Context.BIND_AUTO_CREATE);
-        }
-        else
-        {
-            // if the discovery service already exists, start discovery
-            startDiscovery();
-        }
-    }
-
-    private void startDiscovery()
-    {
-        if (mArdiscoveryService != null)
-        {
-            mArdiscoveryService.start();
-        }
-    }
-
-    private void registerReceivers()
-    {
-         receiver = new ARDiscoveryServicesDevicesListUpdatedReceiver(mDiscoveryDelegate);
-        LocalBroadcastManager localBroadcastMgr = LocalBroadcastManager.getInstance(context);
-        localBroadcastMgr.registerReceiver(receiver,
-                new IntentFilter(ARDiscoveryService.kARDiscoveryServiceNotificationServicesDevicesListUpdated));
-    }
-
-
-    private ARDiscoveryDevice createDiscoveryDevice(@NonNull ARDiscoveryDeviceService service) {
-        ARDiscoveryDevice device = null;
+        ARDeviceController deviceController = null;
         try {
-            device = new ARDiscoveryDevice(context, service);
-        } catch (ARDiscoveryException e) {
-            Log.e(TAG, "Exception", e);
+            deviceController = new ARDeviceController(device);
+        } catch (ARControllerException e) {
+            e.printStackTrace();
         }
+        return deviceController;
+    }
 
+    private ARDiscoveryDevice createDiscoveryDevice()
+    {
+        ARDiscoveryDevice device = null;
+        if ((mService != null))
+        {
+            try
+            {
+                device = new ARDiscoveryDevice();
+
+                ARDiscoveryDeviceNetService netDeviceService = (ARDiscoveryDeviceNetService) mService.getDevice();
+
+                device.initWifi(ARDISCOVERY_PRODUCT_ENUM.ARDISCOVERY_PRODUCT_ARDRONE, netDeviceService.getName(), netDeviceService.getIp(), netDeviceService.getPort());
+            }
+            catch (ARDiscoveryException e)
+            {
+                e.printStackTrace();
+                Log.e(TAG, "Error: " + e.getError());
+            }
+        }
+        else{
+            Log.e(TAG,"No Device created");
+        }
         return device;
     }
 
-    private void unregisterReceivers()
+    private ARCOMMANDS_ARDRONE3_PILOTINGSTATE_FLYINGSTATECHANGED_STATE_ENUM getPilotingState()
     {
-        LocalBroadcastManager localBroadcastMgr = LocalBroadcastManager.getInstance(context);
-        localBroadcastMgr.unregisterReceiver(receiver);
-    }
-
-    private void closeServices()
-    {
-        Log.d(TAG, "closeServices ...");
-
-        if (mArdiscoveryService != null)
+        ARCOMMANDS_ARDRONE3_PILOTINGSTATE_FLYINGSTATECHANGED_STATE_ENUM flyingState = ARCOMMANDS_ARDRONE3_PILOTINGSTATE_FLYINGSTATECHANGED_STATE_ENUM.eARCOMMANDS_ARDRONE3_PILOTINGSTATE_FLYINGSTATECHANGED_STATE_UNKNOWN_ENUM_VALUE;
+        if (mDroneController != null)
         {
-            new Thread(new Runnable() {
-                @Override
-                public void run()
+            try
+            {
+                ARControllerDictionary dict = mDroneController.getCommandElements(ARCONTROLLER_DICTIONARY_KEY_ENUM.ARCONTROLLER_DICTIONARY_KEY_ARDRONE3_PILOTINGSTATE_FLYINGSTATECHANGED);
+                if (dict != null)
                 {
-                    mArdiscoveryService.stop();
-
-                    context.unbindService(mArdiscoveryServiceConnection);
-                    mArdiscoveryService = null;
+                    ARControllerArgumentDictionary<Object> args = dict.get(ARControllerDictionary.ARCONTROLLER_DICTIONARY_SINGLE_KEY);
+                    if (args != null)
+                    {
+                        Integer flyingStateInt = (Integer) args.get(ARFeatureARDrone3.ARCONTROLLER_DICTIONARY_KEY_ARDRONE3_PILOTINGSTATE_FLYINGSTATECHANGED_STATE);
+                        flyingState = ARCOMMANDS_ARDRONE3_PILOTINGSTATE_FLYINGSTATECHANGED_STATE_ENUM.getFromValue(flyingStateInt);
+                    }
                 }
-            }).start();
+            }
+            catch (ARControllerException e)
+            {
+                e.printStackTrace();
+            }
         }
+        return flyingState;
     }
-    */
-
-
 
 }
